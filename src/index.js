a006561ebe214af8cdf19882de9c397442794a6a
katalinags <katest2599@gmail.com>
2022-02-26 15:17:59 -0300

    Added the "time ago" component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import TimeAgo from 'javascript-time-ago'
+import time from 'javascript-time-ago/locale/en-UG.json'
+
+TimeAgo.addDefaultLocale(time)
 
 ReactDOM.render(
   <React.StrictMode>
